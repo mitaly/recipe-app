b221ed5d917c9d1f529e69276762b41c954731f4
mitaly <senmitaly13@gmail.com>
2019-09-18 22:37:24 +0530

    recipe model, list, details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.recipes.push(new Recipe("Pasta", "Red sauce pasta", "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg"));
+    this.recipes.push(new Recipe("Chicken", "Masala chicken", "https://img.delicious.com.au/bsUYtcs3/w759-h506-cfill/del/2019/02/zucchini-spaghetti-with-bolognese-101168-2.jpg"));
   }
 
 }
